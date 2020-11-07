00a61e6161541e5979d366f8751b2e09e0e6c9be
Elias <fahimermo@gmail.com>
2020-11-07 13:45:18 +0100

    Init React project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/bootstrap.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'jquery/'
 
 ReactDOM.render(
   <React.StrictMode>
